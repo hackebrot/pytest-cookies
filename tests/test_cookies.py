e48bbb43e9813aa50b93c008ae1a28ec5a100881
Raphael Pierzina <raphael@hackebrot.de>
2017-08-12 12:45:16 +0200

    Add python source code encoding to tmp test files

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -10,6 +10,8 @@ def test_cookies_fixture(testdir):
 
     # create a temporary pytest test module
     testdir.makepyfile("""
+        # -*- coding: utf-8 -*-
+
         def test_valid_fixture(cookies):
             assert hasattr(cookies, 'bake')
             assert callable(cookies.bake)
@@ -33,7 +35,7 @@ def cookiecutter_template(tmpdir):
 
     template_config = {
         'repo_name': 'foobar',
-        'short_description': 'Test Project'
+        'short_description': 'Test Project',
     }
     template.join('cookiecutter.json').write(json.dumps(template_config))
 
@@ -52,6 +54,8 @@ def cookiecutter_template(tmpdir):
 def test_cookies_bake_with_template_kwarg(testdir, cookiecutter_template):
     """bake accepts a template kwarg."""
     testdir.makepyfile("""
+        # -*- coding: utf-8 -*-
+
         def test_bake_project(cookies):
             result = cookies.bake(
                 extra_context={'repo_name': 'helloworld'},
@@ -75,10 +79,14 @@ def test_cookies_bake_with_template_kwarg(testdir, cookiecutter_template):
 
 
 def test_cookies_bake_template_kwarg_overrides_cli_option(
-    testdir, cookiecutter_template
+    testdir,
+    cookiecutter_template,
 ):
     """bake template kwarg overrides cli option."""
+
     testdir.makepyfile("""
+        # -*- coding: utf-8 -*-
+
         def test_bake_project(cookies):
             result = cookies.bake(
                 extra_context={'repo_name': 'helloworld'},
@@ -107,6 +115,8 @@ def test_cookies_bake(testdir, cookiecutter_template):
     create a project from it.
     """
     testdir.makepyfile("""
+        # -*- coding: utf-8 -*-
+
         def test_bake_project(cookies):
             result = cookies.bake(extra_context={'repo_name': 'helloworld'})
 
@@ -135,6 +145,8 @@ def test_cookies_bake_should_create_new_output_directories(
     create a project from it.
     """
     testdir.makepyfile("""
+        # -*- coding: utf-8 -*-
+
         def test_bake_should_create_new_output(cookies):
             first_result = cookies.bake()
             assert first_result.exception is None
@@ -172,6 +184,8 @@ def test_cookies_bake_should_handle_exception(testdir):
     template.ensure('cookiecutter.repo_name', dir=True)
 
     testdir.makepyfile("""
+        # -*- coding: utf-8 -*-
+
         def test_bake_should_fail(cookies):
             result = cookies.bake()
 
