31ed685eccc0660ede384a81f7efa3dd477edcb9
Raphael Pierzina <raphael@hackebrot.de>
2019-05-22 07:26:29 +0200

    Add Python 3.7 to setup

--- setup.py
+++ setup.py
@@ -8,46 +8,40 @@ from setuptools import setup
 
 def read(fname):
     file_path = os.path.join(os.path.dirname(__file__), fname)
-    return codecs.open(file_path, encoding='utf-8').read()
+    return codecs.open(file_path, encoding="utf-8").read()
 
 
 setup(
-    name='pytest-cookies',
-    version='0.3.0',
-    author='Raphael Pierzina',
-    author_email='raphael@hackebrot.de',
-    maintainer='Raphael Pierzina',
-    maintainer_email='raphael@hackebrot.de',
-    license='MIT',
-    url='https://github.com/hackebrot/pytest-cookies',
-    description='A Pytest plugin for your Cookiecutter templates',
-    long_description=read('README.rst'),
-    py_modules=['pytest_cookies'],
+    name="pytest-cookies",
+    version="0.3.0",
+    author="Raphael Pierzina",
+    author_email="raphael@hackebrot.de",
+    maintainer="Raphael Pierzina",
+    maintainer_email="raphael@hackebrot.de",
+    license="MIT",
+    url="https://github.com/hackebrot/pytest-cookies",
+    description="A Pytest plugin for your Cookiecutter templates",
+    long_description=read("README.rst"),
+    py_modules=["pytest_cookies"],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
-    install_requires=[
-        'pytest>=3.3.0',
-        'cookiecutter>=1.4.0'
-    ],
+    install_requires=["pytest>=3.3.0", "cookiecutter>=1.4.0"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Testing',
-        'Framework :: Pytest',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Testing",
+        "Framework :: Pytest",
     ],
-    entry_points={
-        'pytest11': [
-            'cookies = pytest_cookies',
-        ],
-    },
-    keywords=['cookiecutter', 'pytest'],
+    entry_points={"pytest11": ["cookies = pytest_cookies"]},
+    keywords=["cookiecutter", "pytest"],
 )
