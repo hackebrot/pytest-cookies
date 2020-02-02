4dd49da8451efd943c55bfbef3c74569bd3300a0
Raphael Pierzina <raphael@hackebrot.de>
2020-02-02 22:51:52 +0100

    Update version to 0.5.0.rc0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setuptools.setup(
     name="pytest-cookies",
-    version="0.4.0",
+    version="0.5.0.rc0",
     author="Raphael Pierzina",
     author_email="raphael@hackebrot.de",
     maintainer="Raphael Pierzina",
@@ -33,9 +33,7 @@ setuptools.setup(
     zip_safe=False,
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     install_requires=[
-        "arrow<0.14.0",
-        "cookiecutter>=1.4.0,<=1.7.0",
-        "pytest>=3.3.0,<6.0.0",
+        "arrow<0.14.0", "cookiecutter>=1.4.0,<=1.7.0", "pytest>=3.3.0,<6.0.0"
     ],
     classifiers=[
         "Development Status :: 4 - Beta",
