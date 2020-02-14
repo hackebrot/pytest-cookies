2af9822fe5e2f08bee036b7bc172c14cb25ca321
James Myatt <james@jamesmyatt.co.uk>
2020-02-14 12:00:41 +0000

    Allow minor releases of cookiecutter

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
     zip_safe=False,
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     install_requires=[
-        "arrow<0.14.0", "cookiecutter>=1.4.0,<=1.7.0", "pytest>=3.3.0,<6.0.0"
+        "arrow<0.14.0", "cookiecutter>=1.4.0,<1.8.0", "pytest>=3.3.0,<6.0.0"
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
