c79421c11758846009ba40f30b5dc227b31ade14
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2021-03-03 17:06:12 -0500

    No pytest 6.2.0 and python 3.5; remove py35

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     install_requires=[
         "arrow<0.14.0", "cookiecutter>=1.4.0,<1.8.0", "pytest>=6.2.0,<7.0.0"
     ],
@@ -41,7 +41,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
