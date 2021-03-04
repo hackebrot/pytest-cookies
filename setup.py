a60d97abaf8812b31efc3b5b6f69f8b695f780ee
Jon Zeolla <Jon.Zeolla@SeisoLLC.com>
2021-03-04 10:53:53 -0500

    Reformat project with black - I see this was done before

--- setup.py
+++ setup.py
@@ -33,7 +33,9 @@ setuptools.setup(
     zip_safe=False,
     python_requires=">=3.6,<3.10",
     install_requires=[
-        "arrow<0.14.0", "cookiecutter>=1.4.0,<1.8.0", "pytest>=6.2.0,<7.0.0"
+        "arrow<0.14.0",
+        "cookiecutter>=1.4.0,<1.8.0",
+        "pytest>=6.2.0,<7.0.0",
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
