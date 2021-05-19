113cf4084b05923068388728e2e5f8111e3653ef
Raphael Pierzina <raphael@hackebrot.de>
2021-05-19 08:27:18 +0200

    Update tests with a check for project_path

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -101,9 +101,13 @@ def test_cookies_bake(testdir, cookiecutter_template):
 
             assert result.exit_code == 0
             assert result.exception is None
+
+            assert result.project_path.name == 'helloworld'
+            assert result.project_path.is_dir()
+            assert str(result) == '<Result {}>'.format(result.project_path)
+
             assert result.project.basename == 'helloworld'
             assert result.project.isdir()
-
             assert str(result) == '<Result {}>'.format(result.project)
     """
     )
